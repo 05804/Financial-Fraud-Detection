5490049bb9f4e9bf13d880aecf9e2b3a1cadf285
Shaik Junaid <shaikjunaid4321@gmail.com>
2026-09-20 12:45:31 +0000

    Add FastAPI fraud prediction API

---
+++ api/main.py
@@ -0,0 +1,67 @@
+from fastapi import FastAPI
+from pydantic import BaseModel
+
+from api.predictor import predict_transaction
+
+
+app = FastAPI(
+    title="Financial Fraud Detection API",
+    description="API for fraud detection and risk prediction",
+    version="1.0.0"
+)
+
+
+class Transaction(BaseModel):
+    Time: float
+    Amount: float
+
+    V1: float
+    V2: float
+    V3: float
+    V4: float
+    V5: float
+    V6: float
+    V7: float
+    V8: float
+    V9: float
+    V10: float
+    V11: float
+    V12: float
+    V13: float
+    V14: float
+    V15: float
+    V16: float
+    V17: float
+    V18: float
+    V19: float
+    V20: float
+    V21: float
+    V22: float
+    V23: float
+    V24: float
+    V25: float
+    V26: float
+    V27: float
+    V28: float
+
+
+@app.get("/")
+def root():
+    return {
+        "message": "Financial Fraud Detection API is running",
+        "status": "healthy"
+    }
+
+
+@app.get("/health")
+def health():
+    return {
+        "status": "healthy"
+    }
+
+
+@app.post("/predict")
+def predict(transaction: Transaction):
+    return predict_transaction(
+        transaction.model_dump()
+    )
---
+++ api/predictor.py
@@ -0,0 +1,60 @@
+import joblib
+import numpy as np
+import pandas as pd
+from pathlib import Path
+
+
+PROJECT_ROOT = Path(__file__).resolve().parent.parent
+MODEL_PATH = PROJECT_ROOT / "models" / "fraud_model.pkl"
+
+
+# Load the model once when the API starts
+model_data = joblib.load(MODEL_PATH)
+
+model = model_data["model"]
+threshold = model_data["threshold"]
+
+
+def predict_transaction(transaction: dict):
+    transaction_df = pd.DataFrame([transaction])
+
+    transaction_df["Amount_Log"] = np.log1p(
+        transaction_df["Amount"]
+    )
+
+    transaction_df["Hour"] = (
+        transaction_df["Time"] // 3600
+    ) % 24
+
+    transaction_df["Hour_Sin"] = np.sin(
+        2 * np.pi * transaction_df["Hour"] / 24
+    )
+
+    transaction_df["Hour_Cos"] = np.cos(
+        2 * np.pi * transaction_df["Hour"] / 24
+    )
+
+    transaction_df = transaction_df[model.feature_names_in_]
+    fraud_probability = model.predict_proba(
+        transaction_df
+    )[:, 1][0]
+
+    prediction = int(
+        fraud_probability >= threshold
+    )
+
+    risk_score = fraud_probability * 100
+
+    if risk_score < 30:
+        risk_level = "Low Risk"
+    elif risk_score < 70:
+        risk_level = "Medium Risk"
+    else:
+        risk_level = "High Risk"
+
+    return {
+        "fraud_probability": round(float(fraud_probability), 4),
+        "risk_score": round(float(risk_score), 2),
+        "risk_level": risk_level,
+        "prediction": "FRAUD" if prediction == 1 else "NORMAL"
+    }
